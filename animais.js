871be114252c847b017d8502a4433bb60a4e8716
MarceloSilveira1709 <marcelosilverag@hotmail.com>
2024-04-08 14:16:44 -0300

    refazendo exercicio

--- animais.js
+++ animais.js
@@ -1,16 +1,26 @@
-function Animal (nome) {
+function Animal(nome,idade) {
     this.nome = nome;
-            
+    this.idade = idade;
 }
 
-function Cachorro (nome, raca,pelo) {
-    this.nome = nome;
+function Cachorro (nome,idade,raca,porte) {
     this.raca = raca;
-    this.pelo = pelo;
-    }
+    this.porte= porte;
 
-function Gato (nome, raca, pelo) {
-    this.nome = nome
-    this.raca = raca;
-    this.pelo = pelo;
+    Animal.call(this, nome,idade);
 }
+
+function Gato ( nome,idade,pelo,temperamento) {
+    this.pelo = pelo;
+    this.temperamento = temperamento;
+    
+    Animal.call(this, nome,idade);
+}
+
+const cachorro = new Cachorro ('Cascao',10,'vira-lata','porte medio');
+const gato1 = new Gato ( 'Luna',2 ,'curto', 'arisca');
+const gato2 = new Gato ('Bolinha', 1 ,'grande', 'manso');
+
+console.log(cachorro)
+console.log(gato1)
+console.log(gato2)
