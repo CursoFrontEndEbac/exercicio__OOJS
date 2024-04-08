b724afbbf330394eb7e27c8e8a973cc772bca339
MarceloSilveira1709 <marcelosilverag@hotmail.com>
2024-04-08 09:44:03 -0300

    subindo exercicio

---
+++ animais.js
@@ -0,0 +1,16 @@
+function Animal (nome) {
+    this.nome = nome;
+            
+}
+
+function Cachorro (nome, raca,pelo) {
+    this.nome = nome;
+    this.raca = raca;
+    this.pelo = pelo;
+    }
+
+function Gato (nome, raca, pelo) {
+    this.nome = nome
+    this.raca = raca;
+    this.pelo = pelo;
+}
